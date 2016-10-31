f3bef3a9b2fb8b961dc83b423324f80bc90a2970
Ariel Godinez <godiarie@siu.edu>
2016-10-31 18:02:20 -0500

    a couple style changes

--- src/Utils.java
+++ src/Utils.java
@@ -3,14 +3,14 @@ public class Utils {
 	// this class will serve to hold utility methods that WebCrawlers have access to
 	
 	public static String trim(String s, int width) {
-        if (s.length() > width)
-            return s.substring(0, width-1) + ".";
-        else
-            return s;
-    }
+		if (s.length() > width)
+			return s.substring(0, width-1) + ".";
+		else
+			return s;
+	}
 	
-    public static void print(String msg, Object... args) {
-        System.out.println(String.format(msg, args));
-    }
-
+	public static void print(String msg, Object... args) {
+		System.out.println(String.format(msg, args));
+	}
+	
 }
